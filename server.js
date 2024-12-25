5016061ac38e14668fa6a596a22678c129577d2f
Bhardwaj-Aniket-000 <paniketbhardwaj676@gmail.com>
2024-12-25 22:47:43 +0530

    whitelist all origin in cors policy

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "https://globalheadlines.netlify.app/",
-    methods: ["get", "post", "delete", "put"],
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
