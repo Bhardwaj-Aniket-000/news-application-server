f006ca689187c51376960a8401550c9dd2f6c444
Bhardwaj-Aniket-000 <paniketbhardwaj676@gmail.com>
2024-12-25 22:36:19 +0530

    whitelist the url of frontend fr cors policy

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://globalheadlines.netlify.app/",
+    methods: ["get", "post", "delete", "put"],
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
