95a245ed4a73106140a14335a62813bf00de20bb
Bhardwaj-Aniket-000 <paniketbhardwaj676@gmail.com>
2024-12-27 22:16:12 +0530

    add middleware to handle cors policy

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const bodyParser = require("body-parser");
 
 app.use(cookieParser());
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "none");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
