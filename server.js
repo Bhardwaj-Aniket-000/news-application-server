45b7bd81460dd39754409ecd993730a587029c46
Bhardwaj-Aniket-000 <paniketbhardwaj676@gmail.com>
2025-04-02 23:35:40 +0530

    remove extra code from server file

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const bodyParser = require("body-parser");
 
 app.use(cookieParser());
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "none");
-  next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
